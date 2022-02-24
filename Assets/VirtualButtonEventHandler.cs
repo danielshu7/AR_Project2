2f5d2366f8a1a3181ded13de36248b7a67678de0
Daniel Shu <danielshu99384@gmail.com>
2022-02-24 11:47:30 -0600

    update

--- Assets/VirtualButtonEventHandler.cs
+++ Assets/VirtualButtonEventHandler.cs
@@ -7,6 +7,7 @@ using Vuforia;
 public class VirtualButtonEventHandler : MonoBehaviour
 {
     public GameObject bookInfo;
+    public GameObject review;
     public VirtualButtonBehaviour Vb;
     // Start is called before the first frame update
     void Start()
@@ -15,15 +16,18 @@ public class VirtualButtonEventHandler : MonoBehaviour
         Vb.RegisterOnButtonReleased(OnButtonReleased);
 
         bookInfo.SetActive(true);
+        review.SetActive(false);
     }
 
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
         bookInfo.SetActive(false);
+        review.SetActive(true);
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
         bookInfo.SetActive(true);
+        review.SetActive(false);
     }
 }
